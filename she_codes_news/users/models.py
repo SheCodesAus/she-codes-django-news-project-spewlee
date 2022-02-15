22b37804e099d31bd65d9966efc6b47533d83377
spewlee <suelee_1@hotmail.com>
2022-02-15 19:37:36 +0800

    Update models.py

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,7 +1,5 @@
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.forms import CharField, ImageField, URLField
 
 # Create your models here.
 class CustomUser (AbstractUser):
