d4f5d838e4a3731c3219190e56013d14b387d4cd
spewlee <suelee_1@hotmail.com>
2022-02-18 15:01:21 +0800

    added the Category and NewsStory models to Admin

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,9 @@
+from http.client import MULTIPLE_CHOICES
 from django.contrib.auth import get_user_model
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
